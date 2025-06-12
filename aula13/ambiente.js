fcac5a2e66bfb65cba16299c3c0ee00c3f3557fc
beatrizkingwell <beatriz.m.kingwell@gmail.com>
2025-06-12 20:52:36 +0100

    voltei ao cursoemvideo

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+/* ESTRUTURA DE CONTROLE: REPETIÇÕES */
+
+var c = 1
+while (c <= 20){
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+/*
+
+ESTRUTURA DE CONTROLE: SEQUÊNCIAS
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
