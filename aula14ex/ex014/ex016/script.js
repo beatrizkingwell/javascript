d6c4043e82750d7a7d54265040c38d71817c54f5
beatrizkingwell <beatriz.m.kingwell@gmail.com>
2025-06-23 09:09:12 +0100

    exercicio novo de javascript

--- aula14ex/ex014/ex016/script.js
+++ aula14ex/ex014/ex016/script.js
@@ -1,27 +1,38 @@
 function contar(){
-    let ini = document.getElementById('txti') /*variavel para a opção inicio*/
-    let fim = document.getElementById ('txtf') /*variavel para a opção fim*/
-    let passo = document.getElementById ('txtp') /*variavel para a opção passo*/
-    let res = document.getElementById ('res') /*Tem uma div no html chamada res, é a div que diz "preparando a contagem"*/
+    let ini = document.getElementById('txti') //variavel para a opção inicio
+    let fim = document.getElementById ('txtf') //variavel para a opção fim
+    let passo = document.getElementById ('txtp') //variavel para a opção passo
+    let res = document.getElementById ('res') //Tem uma div no html chamada res, é a div que diz "preparando a contagem"
 
-    if(ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
     } else {
         res.innerHTML = 'Contando: '
-        let i = Number(ini.value) /*Vou pegar no valor que está dentro de da variavel ini e converter num número*/
-        let f = Number(fim.value)   /*Vou pegar no valor que está dentro de da variavel fim e converter num número*/
-        let p = Number(passo.value) /*Vou pegar no valor que está dentro de da variavel passo e converter num número*/
+        let i = Number(ini.value) //Vou pegar no valor que está dentro de da variavel ini e converter num número
+        let f = Number(fim.value)   //Vou pegar no valor que está dentro de da variavel fim e converter num número
+        let p = Number(passo.value) //Vou pegar no valor que está dentro de da variavel passo e converter num número
+        if (p <=0) {    //Se o passo for menor ou igual a 0, será considerado que passo é igual a 1
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
 
-        for(let c = i; c <= f; c += p){ /* O contador vai começar no início, enquanto o contador for menor ou igual ao fim, o contador irá receber ele mesmo mais o passo */
-            res.innerHTML += ` ${C} \u{1F449}`
-        } 
-        res.innerHTML += `\u{1F3C1}`
-    }
+        if (i < f) {
+            for(let c = i; c <= f; c += p){ // [CONTAGEM CRESCENTE] O contador vai começar no início, enquanto o contador for menor ou igual ao fim, o contador irá receber ele mesmo mais o passo
+            res.innerHTML += ` ${c} \u{1F449}`
+            } 
 
-    /*Se as varíaveis início ou fim ou passo estiverem vazios, aparecerá a mensagem de alerta de erro, se não estiverem vazios, aparecerá o alerta "tudo ok"*/
+        } else {
+            for (let c = i; c>= f; c -= p){  // [CONTAGEM REGRESSIVA]
+            res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += ` \u{1F3C1}`
+        }
+
+    //Se as varíaveis início ou fim ou passo estiverem vazios, aparecerá a mensagem de alerta de erro, se não estiverem vazios, aparecerá o alerta "tudo ok"
 }
 
-/* let e var têm efeitos muito similares, muda mais a nível de escopo
-
-os nomes dos ids podem ser colocados em aspas simples ( ' ' ) ou aspas duplas (" ")*/
+// let e var têm efeitos muito similares, muda mais a nível de escopo
+// os nomes dos ids podem ser colocados em aspas simples ( ' ' ) ou aspas duplas (" ")
 
--- aula14ex/ex014/ex017/script.js
+++ aula14ex/ex014/ex017/script.js
@@ -1,2 +1,20 @@
-
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número') //Caso não seja colocado nenhum número, aparecerá este aviso
+    } else {
+        let n = Number(num.value) //O valor de n depende do que o usuário colocar
+        let c = 1
+        tab.innerHTML = '' //Antes de mostrar a tabuada, eu limpo a área de tabuada
+        while (c <= 10){ //O valor de c vai de 1 até 10
+            let item = document.createElement ('option')
+            item.text = `${n} x ${c} = ${n*c}` //A multiplicação do número com a contagem até 10, ex: 5x1=5 até 5x10=50
+            item.value = `tab${c}` //Saber qual item foi selecionado
+            tab.appendChild(item)
+            c++ //Depois da multiplicação é acrescentado +1 ao c até a tabuada ir até n x 10
+        }
+    }
+}
+    
 
